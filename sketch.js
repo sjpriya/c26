6e23ab242b1034b142cc9ac18ab72399cb0133a4
Priya SJ <sjpriya2020@gmail.com>
2020-11-28 17:19:45 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new Ground(150,305,300,170);
 
 }
 
@@ -53,6 +54,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    platform.display();
 
     box5.display();
     log4.display();
